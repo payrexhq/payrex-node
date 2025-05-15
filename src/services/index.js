c0d476a400698c54fd5155697733371d63a7ba33
Jaime Hing III <jaimehing@gmail.com>
2025-05-16 07:06:41 +0800

    feat: add get payment by id endpoint.

--- src/services/index.js
+++ src/services/index.js
@@ -4,12 +4,14 @@ const CheckoutSessionService = require('./CheckoutSessionService');
 const CustomerSessionService = require('./CustomerSessionService');
 const CustomerService = require('./CustomerService');
 const PaymentIntentService = require('./PaymentIntentService');
+const PaymentService = require('./PaymentService');
 const RefundService = require('./RefundService');
 const WebhookService = require('./WebhookService');
 
 module.exports = {
   checkoutSessions: CheckoutSessionService,
   customerSessions: CustomerSessionService,
+  payments: PaymentService,
   paymentIntents: PaymentIntentService,
   customers: CustomerService,
   billingStatements: BillingStatementService,
